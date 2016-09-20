a5c969692744aecb60d261ebdc720dea7da850f3
liuweiqin <772863869@qq.com>
2016-09-20 23:54:22 +0800

    提供简单的路由信息，和简单的视图

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -3,9 +3,47 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '主页' });
 });
 
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+	res.render('login', { title: '登录'});
+});
 
+/* POST 登录 login */
+router.post('/login', function(req, res, next) {
+	res.render('index', { title: '主页' });
+});
+
+/* GET 注册页面 */
+router.get('/reg', function(req, res, next) {
+	res.render('reg', { title: '注册'});
+});
+
+/* POST 注册页面 */
+router.post('/reg', function(req, res, next) {
+	res.render('index', { title: '主页'});
+});
+
+/* GET 发表页面,获取所有的文章列表 */
+router.get('/post', function(req, res, next) {
+	res.render('postlist', { title: '发表的文章' });
+});
+
+/* POST 发表页面 */
+router.post('/post', function(req, res, next) {
+	res.render('post', { title: '发表' });
+});
+
+/* GET 登出 */
+router.get('/logout', function(req, res, next) {
+	res.render('index', { title: '主页' });
+});
+
+/* post 登出 */
+router.post('/logout', function(req, res, next) {
+	res.render('index', { title: '主页' });
+});
 
 module.exports = router;
