8a6fab5f7ba0e7695f3fcffc430ad7e28140a3cb
liuweiqin <772863869@qq.com>
2016-09-22 22:50:00 +0800

    修改模板页信息，注意空格和tab键的使用

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '主页' });
+  res.render('index', { title: '这是主页' });
 });
 
 /* GET login page. */
