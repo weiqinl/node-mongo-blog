efe59ac51fae4a497f7db91b4530ecc86f962f78
liuweiqin <772863869@qq.com>
2016-09-26 01:41:29 +0800

    注册

--- blog/models/db.js
+++ blog/models/db.js
@@ -6,7 +6,7 @@ var Server = require('mongodb').Server;
 // 设置数据库名、数据库地址和数据库端口创建了一个数据库连接实例，
 // 并通过  module.exports 导出该实例
 module.exports = new Db(
-	Settings.db, 
+	settings.db, 
 	new Server(settings.host, settings.port), 
 	{	safe: true}
 	);
--- blog/routes/index.js
+++ blog/routes/index.js
@@ -1,8 +1,8 @@
-var crypto = require('crypto');// 用它来生成散列值来加密密码
-var User = require('../models/user.js');
-
 var express = require('express');
 var router = express.Router();
+
+var crypto = require('crypto');// 用它来生成散列值来加密密码
+var User = require('../models/user.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -26,7 +26,45 @@ router.get('/reg', function(req, res, next) {
 
 /* POST 注册页面 */
 router.post('/reg', function(req, res, next) {
-	res.render('index', { title: '主页'});
+	
+	var name = req.body.name;
+	var password = req.body.password;
+	var password_re = req.body['password-repeat'];
+	//检验用户两次输入的密码是否一致
+	if (password_re != password) {
+		req.flash('error', '两次输入的密码不一致！');
+		return res.redirect('/reg');//重定向功能。返回注册页
+	}
+	//生成密码的md5值
+	var md5 = crypto.createHash('md5');
+	var newPassword = md5.update(req.body.password).digest('hex');
+	var newUser = new User({
+		name: name,
+		password: newPassword,
+		email: req.body.email
+	});
+	//检查用户名是否已经存在
+	User.get(newUser.name, function(err, user) {
+		if(err) {
+			req.flash('error', err);
+			return res.redirect('/');
+		}
+		if (user) {
+			req.flash('error', '用户名已经存在!');
+			return res.redirect('/reg');//返回注册页面;
+		}
+		//如果不存在则新增用户
+		newUser.save(function(err, user) {
+			if (err) {
+				req.flash('error', err);
+				return res.redirect('/reg');//注册失败返回注册页面
+			}
+			// 我们把用户信息存储在了 session 里，以后就可以通过 req.session.user 读取用户信息
+			req.session.user = newUser;//用户信息存入session
+			req.flash('success', '注册成功!');
+			res.redirect('/'); //注册成功后返回主页
+		});
+	});		
 });
 
 /* GET 发表页面,获取所有的文章列表 */
